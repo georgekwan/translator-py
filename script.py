881540ccf3601872f9ada5af764e97808238a0d7
George <gwmkwan@gmail.com>
2023-07-03 12:18:18 -0600

    chore: rename txt files

--- script.py
+++ script.py
@@ -3,11 +3,11 @@ from translate import Translator
 translator = Translator(to_lang="ko")
 
 try:
-    with open('./test.txt', mode='r') as my_file:
+    with open('./test-input.txt', mode='r') as my_file:
         text = my_file.read()
         translation = translator.translate(text)
         # Access the translated text using the 'text' attribute
-        with open('./test-ko.txt', mode='w', encoding='utf-8') as my_file2:
+        with open('./test-output.txt', mode='w', encoding='utf-8') as my_file2:
             my_file2.write(translation)
 except FileNotFoundError as e:
     print('check your file path silly!')
