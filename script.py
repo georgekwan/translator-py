7843018aa95164caf68109b609576f9ed3bb28c4
George <gwmkwan@gmail.com>
2023-07-03 12:03:18 -0600

    feat: install translator library

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
+from translate import Translator
+
+translator = Translator(to_lang="ja")
+
 try:
     with open('./test.txt', mode='r') as my_file:
-        print(my_file.read())
+        text = my_file.read()
+        translation = translator.translate(text)
+        print(translation)
 except FileNotFoundError as e:
     print('check your file path silly!')
